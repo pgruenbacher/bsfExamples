f1885fc27ff6f20411be81c8b9a468420d3b2f78
paul gruenbacher <pgruenbacher@totalsim.us>
2019-07-17 17:00:02 -0500

    s

--- Source/SkeletalAnimation/Main.cpp
+++ Source/SkeletalAnimation/Main.cpp
@@ -35,7 +35,7 @@
 namespace bs
 {
 	SPtr<ct::VertexBuffer> gInstanceBuffer;
-	UINT32 gNumInstances = 1000;
+	const UINT32 gNumInstances = 1000;
 
 	UINT32 windowResWidth = 1280;
 	UINT32 windowResHeight = 720;
@@ -52,6 +52,8 @@ namespace bs
 		HTexture exampleSkyCubemap;
 		HMaterial exampleMaterial;
 	};
+
+	constexpr float FPS = 10;
 
 	template<typename TCurveVector>
 	void calcTimeRange(float& min, float& max, const TCurveVector& curves) {
@@ -75,25 +77,12 @@ namespace bs
 
 	struct Data {
 		Vector3 position;
-		uint frame;
-		uint frame1;
-		uint frame2;
-		uint frame3;
+		float frame;
+		float frame1;
+		float frame2;
+		float frame3;
 	};
 
-	void updateInstancing(UINT32 frame) {
-
-		Data data[gNumInstances];
-		for (UINT32 i = 0; i < gNumInstances; ++i) {
-			data[i].position = Vector3(i % 100, 0, i / 100);
-			UINT32 frameOffset = (frame + i * 7) / 40;
-			data[i].frame = (i + frameOffset) % 15;
-			if (i == 0) {
-				std::cout << "FRAME? " << data[i].frame << std::endl;
-			}
-		}
-		gInstanceBuffer->writeData(0, sizeof(data), data, BWT_NORMAL);
-	}
 
 	void setupInstancing(Assets& assets) {
 
@@ -110,7 +99,7 @@ namespace bs
 			 1  // Instance step rate of 1 means the new element will be fetched from the vertex buffer for each drawn instance
 		 );
 		vertexDesc->addVertElem(
-			 VET_UINT4, // Each entry in the instance vertex buffer is a 3D float
+			 VET_FLOAT4, // Each entry in the instance vertex buffer is a 3D float
 			 VES_COLOR, // We map it to the position semantic as vertex shader input
 			 1, // We use the semantic index 1, as 0th index is taken by per-vertex VES_POSITION semantic
 			 1, // We use the second bound vertex buffer for instance data
@@ -130,7 +119,7 @@ namespace bs
 		Data data[gNumInstances];
 		for (UINT32 i = 0; i < gNumInstances; ++i) {
 			data[i].position = Vector3(i % 100, 0, i / 100);
-			data[i].frame = i % 15;
+			data[i].frame = 0;
 		}
 		assert(sizeof(Data) == vbDesc.vertexSize);
 		gInstanceBuffer->writeData(0, sizeof(data), data, BWT_NORMAL);
@@ -160,22 +149,6 @@ namespace bs
 		assert(transform[3][1] == 0.f);
 		assert(transform[3][2] == 0.f);
 		assert(transform[3][3] == 1.f);
-		// colors[0][0] = basis[0][0];
-		// colors[0][1] = basis[0][1];
-		// colors[0][2] = basis[0][2];
-		// colors[0][3] = position.x;
-		// colors[1][0] = basis[1][0];
-		// colors[1][1] = basis[1][1];
-		// colors[1][2] = basis[1][2];
-		// colors[1][3] = position.y;
-		// colors[2][0] = basis[2][0];
-		// colors[2][1] = basis[2][1];
-		// colors[2][2] = basis[2][2];
-		// colors[2][3] = position.z;
-
-		// std::cout << "COL " << colors[0].r << " " << colors[1].g << " " << colors[1].b << std::endl;
-		// std::cout << "COL " << colors[1].r << " " << colors[1].g << " " << colors[1].b << std::endl;
-		// std::cout << "COL " << colors[2].r << " " << colors[2].g << " " << colors[2].b << std::endl;
 	}
 
 	HTexture getSkeletonBoneTransforms(SPtr<Skeleton> skel, HAnimationClip clip) {
@@ -188,10 +161,11 @@ namespace bs
 		// Copy transforms from mapped scene objects
 		UINT32 boneTfrmIdx = 0;
 
-		std::pair<float, float> range = getTimeRange(clip->getCurves());
-		float start = range.first;
-		float end = range.second;
-		UINT32 frames = (end - start) / 0.1f;
+		// std::pair<float, float> range = getTimeRange(clip->getCurves());
+		// float start = range.first;
+		// float end = range.second;
+		// UINT32 frames = (end - start) * FPS;
+		UINT32 frames = clip->getLength() * FPS;
 		assert(frames > 1);
 		// UINT32 frames = 1;
 		// 3 pixels per bone transform.
@@ -205,11 +179,11 @@ namespace bs
 		// Animate bones
 		// UINT32 frame = 0;
 		std::cout << "NUMB BONES " << numBones << std::endl;
-		std::cout << " NUM FRAMES " << frames <<  std::endl;
+		std::cout << " NUM FRAMES " << frames << " " << clip->getLength() << " " << clip->getSampleRate() <<  std::endl;
 		for (UINT32 frame = 0; frame < frames; ++frame) {
 			// have to set hasOverride to false all manually.
 			memset(localPose.hasOverride, 0, sizeof(bool) * localPose.numBones);
-			float time = start + 0.1f * frame;
+			float time = frame / FPS;
 			skel->getPose(transforms.data(), localPose, mask, *clip, time, loop);
 
 			for (UINT32 i = 0; i < transforms.size(); ++i) {
@@ -377,14 +351,33 @@ namespace bs
 	}
 
 	class AnimApplication : public Application {
-		UINT32 mFrame{0};
+		Data mData[gNumInstances];
 	public:
-		AnimApplication(START_UP_DESC d) : Application(d) {}
+		AnimApplication(START_UP_DESC d) : Application(d) {
+
+			for (UINT32 i = 0; i < gNumInstances; ++i) {
+				mData[i].position = Vector3(i % 100, 0, i / 100);
+				UINT32 frameOffset = (i * 7) / 40;
+				mData[i].frame = (frameOffset) % 14;
+			}
+		}
+
+		void updateInstancing(float time) {
+
+			for (UINT32 i = 0; i < gNumInstances; ++i) {
+				mData[i].frame += time * FPS;
+				if (mData[i].frame >= 16.f) {
+					mData[i].frame = mData[i].frame - 16.f;
+				}
+				// mData[i].frame = 15.f;
+				if (i == 0) std::cout << "FRAME ? " << mData[i].frame << std::endl;
+			}
+			gInstanceBuffer->writeData(0, sizeof(mData), mData, BWT_NORMAL);
+		}
 
 		void preUpdate() override {
 			Application::preUpdate();
-			++mFrame;
-			gCoreThread().queueCommand(std::bind(updateInstancing, mFrame));
+			gCoreThread().queueCommand(std::bind(&AnimApplication::updateInstancing, this, gTime().getFrameDelta()));
 		}
 	};
 } // namespace bs
